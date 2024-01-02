d50a776957efc79b1fb5475b4296699e959abfa9
Luke Addison <lukeaddison785@gmail.com>
2024-01-02 15:38:40 +0000

    Remove unused scheme builder

--- pkg/api/v1alpha1/register.go
+++ pkg/api/v1alpha1/register.go
@@ -1,22 +1,9 @@
 package v1alpha1
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 const GroupName = "cost-manager.io"
 
 var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: "v1alpha1"}
-
-var (
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	AddToScheme   = SchemeBuilder.AddToScheme
-)
-
-func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
-		&CostManagerConfiguration{},
-	)
-	return nil
-}

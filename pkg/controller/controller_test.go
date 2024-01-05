69cc5962d07af6d61aa07e85c0f80a45e5bcd0d9
Luke Addison <lukeaddison785@gmail.com>
2024-01-05 04:19:32 +0000

    Improve setup manager unit tests

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -13,21 +13,50 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-func TestSetupAllControllersWithManager(t *testing.T) {
-	// Create manager
-	scheme, err := kubernetes.NewScheme()
-	require.Nil(t, err)
-	mgr, err := ctrl.NewManager(&rest.Config{}, manager.Options{Scheme: scheme})
-	require.Nil(t, err)
-
-	// Setup manager with all controllers...
-	config := &v1alpha1.CostManagerConfiguration{
-		CloudProvider: v1alpha1.CloudProvider{
-			Name: cloudprovider.FakeCloudProviderName,
+func TestSetupWithManager(t *testing.T) {
+	tests := map[string]struct {
+		config        *v1alpha1.CostManagerConfiguration
+		shouldSucceed bool
+	}{
+		"allControllers": {
+			config: &v1alpha1.CostManagerConfiguration{
+				CloudProvider: v1alpha1.CloudProvider{
+					Name: cloudprovider.FakeCloudProviderName,
+				},
+				Controllers: AllControllerNames,
+			},
+			shouldSucceed: true,
 		},
-		Controllers: AllControllerNames,
+		"allControllersWithoutCloudProvider": {
+			config: &v1alpha1.CostManagerConfiguration{
+				Controllers: AllControllerNames,
+			},
+			shouldSucceed: false,
+		},
+		"withoutCloudProvider": {
+			// Configure a controller that does not interact with the cloud provider
+			config: &v1alpha1.CostManagerConfiguration{
+				Controllers: []string{podSafeToEvictAnnotatorControllerName},
+			},
+			shouldSucceed: true,
+		},
 	}
-	err = SetupWithManager(context.Background(), mgr, config)
-	// ...and make sure there are no errors
-	require.Nil(t, err)
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// Create manager
+			scheme, err := kubernetes.NewScheme()
+			require.Nil(t, err)
+			mgr, err := ctrl.NewManager(&rest.Config{}, manager.Options{Scheme: scheme})
+			require.Nil(t, err)
+
+			// Setup manager...
+			err = SetupWithManager(context.Background(), mgr, test.config)
+			// ...and verify success
+			if test.shouldSucceed {
+				require.Nil(t, err)
+			} else {
+				require.NotNil(t, err)
+			}
+		})
+	}
 }

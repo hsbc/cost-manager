de837c2b05fa40b4c9054487f003bb90255dfe00
Luke Addison <lukeaddison785@gmail.com>
2024-12-20 19:12:42 +0000

    Reduce code

--- pkg/kubernetes/node.go
+++ pkg/kubernetes/node.go
@@ -1,8 +1,7 @@
 package kubernetes
 
 const (
-	// https://github.com/kubernetes/autoscaler/blob/5bf33b23f2bcf5f9c8ccaf99d445e25366ee7f40/cluster-autoscaler/utils/taints/taints.go#L39-L40
-	ToBeDeletedTaint = "ToBeDeletedByClusterAutoscaler"
-	// https://github.com/kubernetes/autoscaler/blob/5bf33b23f2bcf5f9c8ccaf99d445e25366ee7f40/cluster-autoscaler/utils/taints/taints.go#L41-L42
+	// https://github.com/kubernetes/autoscaler/blob/5bf33b23f2bcf5f9c8ccaf99d445e25366ee7f40/cluster-autoscaler/utils/taints/taints.go#L39-L42
+	ToBeDeletedTaint       = "ToBeDeletedByClusterAutoscaler"
 	DeletionCandidateTaint = "DeletionCandidateOfClusterAutoscaler"
 )
